f084a541f246f9145a8f06a2ff174f4800543d56
I7RANK <franfgh2000@hotmail.com>
2025-10-09 17:51:45 -0500

    feat: add home and pokemon view to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/Home/HomeView.vue'
+import PokemonView from '@/views/Pokemon/PokemonView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/pokemon',
+      name: 'pokemon',
+      component: PokemonView,
+    },
+  ],
 })
 
 export default router
